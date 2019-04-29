2e4e4f7831f6fac70f5d3e0d1d0a38c5fa45130f
Oke Uguru <ondukwe@gmail.com>
2019-04-28 20:43:43 -0400

    added mongoose connect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const Joi = require('joi');
 const express = require('express');
 const app = express();
@@ -5,6 +6,8 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 const config = require('config')
 const genres = require('./routes/genres')
+
+mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres)
