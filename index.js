d22e2d824c4d5f41045d218e067167f38ce8d42e
Oke Uguru <ondukwe@gmail.com>
2019-04-29 23:37:46 -0400

    added customer data

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
-const Joi = require('joi');
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const helmet = require('helmet')
 const config = require('config')
 const genres = require('./routes/genres')
+const customers = require('./routes/genres')
 
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+    .then(() => console.log("connected to Mongo"))
+    .catch(err => console.log("Could not connect to MongoDB"))
 
 app.use(express.json());
 app.use('/api/genres', genres)
+app.use('/api/customers', customers)
 app.use('/', (req, res) => {
     res.send("Welcome")
 })
